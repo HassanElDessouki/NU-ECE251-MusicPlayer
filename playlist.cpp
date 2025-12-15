c78d3e3affeeb71a9529d460aa6725f92f110e02
Hassan ElDessouki <hassan@eldessouki.com>
2025-12-15 07:59:47 +0300

    Delete Song from Playlist

--- playlist.cpp
+++ playlist.cpp
@@ -22,10 +22,44 @@ void Playlist::insertSong(Song *song) {
 	}
 }
 
-		// getting Information
-		bool isEmpty() const { return size == 0; }
-		int getTotalDuration() const;
-
+void Playlist::deleteSong(Song *song) {
+	// if head is null, print message to user that the list is empty
+	if (head == nullptr) {
+		cout << "List is empty!" << endl;
+		return;
+	}
+	// check if it's the only node
+	if (head->next == head && head->song == song) {
+		delete head;
+		head = nullptr;
+		return;
+	}
+	// check if head is the node to delete
+	if (head->song == song) {                // if so, then the next node after the head becomes the new head
+		PlaylistNode *newHead = head->next;         // first store the memory address of the next node
+		newHead->prev = head->prev;         // set the previous of the newHead to be the last node
+		head->prev->next = newHead;         // set the next of the last node in the list to the newHead
+		delete head;                        // then delete the current head
+		head = newHead;                     // then set the head as the memory address of the next node
+		return;
+	}
+	// otherwise, serarch for the node by traversing it
+	PlaylistNode *currentNode = head;
+	PlaylistNode *toDeleteNode = nullptr;
+	do {
+		if (currentNode->song == song) {
+			toDeleteNode = currentNode;
+		}
+		currentNode = currentNode->next;
+	} while (currentNode != head);
+	if (toDeleteNode == nullptr) {
+		cout << "Song node " << song->getTitle() << " does not exist" << endl;
+	} else {
+		toDeleteNode->prev->next = toDeleteNode->next;
+		toDeleteNode->next->prev = toDeleteNode->prev;
+		delete toDeleteNode;
+	}
+}
 
 		// displaying playlist
 		void displayPlaylist() const;
