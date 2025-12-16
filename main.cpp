8806838bd6a1396e28a0bce8f67907d4ae9019d8
Hassan ElDessouki <hassan@eldessouki.com>
2025-12-16 18:04:27 +0300

    Different Testing Commands

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include "include/song.h"
+#include "include/playlist.h"
+#include "include/playlistutils.h"
 using namespace std;
 
 int main() {
-    Song s("C:/Users/Hassan/Music/Ace of Base - Cruel Summer.mp3");
-    s.loadMetadata();
-    s.displayInfo();
-    s.playSong();
+    // Song s("C:/Users/Hassan/Music/Britney Spears - ...Baby One More Time.mp3");
+    // s.loadMetadata();
+    // s.displayInfo();
+    // s.playSong();
+    // Playlist general("General", "G:/Projects/Academic/music-player/general.csv");
+    // general.loadPlaylist();
+    // general.displayPlaylist();
+    // general.importFolder("C:/Users/Hassan/Music");
+    // // general.play();
+    // // general.savePlaylist();
+    // createPlaylist("MAX Mix1");
+    // listAllPlaylists();
+    // createPlaylist("MAX Mix2");
+    // listAllPlaylists();
+    // playlistExists("MAX Mix1");
+    Playlist* playlist = createPlaylist("MAX Mix");
+    if (playlist != nullptr) {
+        playlist->importFolder("C:/Users/Hassan/Music");
+        playlist->displayPlaylist();
+        playlist->play();
+    }
+
     return 0;
 }
